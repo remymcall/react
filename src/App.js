c2a6c1ce8eb259d780a3581effe94fb1445dde6b
remymcall <remyroick6@gmail.com>
2023-03-01 23:27:14 +0200

    my-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+{/* import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+ constructor(){
+  super()
+  this.state ={
+    user: [],
+    searchBox:''
+  }
+ }
+ componentDidMount(){
+  fetch('https://course-api.com/react-store-products')
+  .then((response)=> response.json())
+  .then(response =>this.setState({user: response}))
+ }
+ searchProduct =(e)=>{
+  this.setState({searchBox: e.target.value})
+ }
+  render(){
+   const filteredItem= this.state.user.filter((user)=>
+   user.name.toLowerCase().includes(this.state.searchBox.toLowerCase())
+   ); 
+   
 
-export default App;
+    return(
+      <div className='App'>
+        <input type='text' placeholder='Make a search here' onChange={(this.searchProduct)}/>
+        {
+          filteredItem.map(user =>{
+            return(
+              <div key={user.id}>
+                <h1>{user.name}</h1>
+                <p>{user.price}</p>
+                <h1>{user.company}</h1>
+              </div>
+            )
+          })
+        }
+        
+        <input type='text' placeholder='type here' onChange={(e)=>{
+      
+          this.setState({user: e.target.value})
+        }}/>
+        <h1>{this.state.user}</h1>
+        <button type='submit'>change here</button> 
+        
+      </div>
+    )
+  }
+}
+export default App;*/}
